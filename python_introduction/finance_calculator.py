8c15ca1d23e5dc37dd53137ee2548fa2c397bb20
HassanElwany <elwany_hassan@hotmail.com>
2024-09-11 10:29:51 +0300

    edit last task

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,39 +1,13 @@
-# Task Description:
-
-# You will create a script named finance_calculator.py. This script will calculate the user’s monthly savings based on inputted monthly income and expenses. It will then project these savings over a year, assuming a fixed interest rate, to demonstrate compound interest’s effect on savings.
-
-# Instructions:
-
-# User Input for Financial Details:
-
-# Prompt the user for their monthly income: “Enter your monthly income: ”.
-# Ask for their total monthly expenses: “Enter your total monthly expenses: ”.
-# Calculate Monthly Savings:
-
-# Calculate the monthly savings by subtracting monthly expenses from the monthly income.
-# Project Annual Savings:
-
-# Assume a simple annual interest rate of 5%.
-# Calculate the projected savings after one year, incorporating the interest. Use the simplified formula for annual savings projection: (Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)).
-# Output Results:
-
-# Display the user’s monthly savings.
-# Display the projected annual savings after including interest.
-# Example Interaction:
-
-# Enter your monthly income: 5000
-# Enter your total monthly expenses: 4000
-# Your monthly savings are $1000.
-# Projected savings after one year, with interest, is: $12600.
-
 monthly_income = int(input('Enter your monthly income: '))
 monthly_expenses = int(input('Enter your total monthly expenses: '))
+
+# Calculate Monthly Savings
 monthly_saving = monthly_income - monthly_expenses
 
-yearly_project_saving = int(monthly_saving * 12 + (monthly_saving * 12 * 0.05))
+# Project Annual Savings
+# Assume a simple annual interest rate of 5%
+yearly_project_saving = monthly_saving * 12 + (monthly_saving * 12 * 0.05)
 
-# print(f'Your monthly savings are ${monthly_saving}.')
-
-# print(f'Projected savings after one year, with interest, is: ${yearly_project_saving}.')
-
-print(f'Your monthly savings are ${monthly_saving}.\nProjected savings after one year, with interest, is: ${yearly_project_saving}.')
+# Output Results
+print(f'Your monthly savings are ${monthly_saving}.')
+print(f'Projected savings after one year, with interest, is: ${yearly_project_saving}.')
