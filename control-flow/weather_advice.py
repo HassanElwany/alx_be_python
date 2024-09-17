ae908805ca96b735c58a454aa8eca4fe141c1ec5
HassanElwany <elwany_hassan@hotmail.com>
2024-09-17 18:34:53 +0300

    0. Weather Recommendation Program 01

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -15,12 +15,11 @@
 
 user_response = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if user_response not in ['sunny', 'rainy', 'cold']:
-    print("Sorry, I don't have recommendations for this weather")
-else:
-    if user_response == 'sunny':
+if user_response == 'sunny':
         print("Wear a t-shirt and sunglasses.")
-    elif user_response == 'rainy':
+elif user_response == 'rainy':
         print("Don't forget your umbrella and a raincoat.")
-    else:
+elif user_response == 'cold':
         print("Make sure to wear a warm coat and a scarf.")
+else:
+        print("Sorry, I don't have recommendations for this weather")
