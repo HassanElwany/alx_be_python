9ab164deb5889520f1a42bf2cfe347a294b5c909
HassanElwany <elwany_hassan@hotmail.com>
2024-09-26 18:18:02 +0300

    fns data structure task

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,26 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num2 * num1
-        case 'divide':
-            try:
-                return num1 / num2
-            except ZeroDivisionError():
-                return f"Can not divide by Zero!"
-        case _:
-            return f"something went wrong"
+    # match operation:
+    #     case 'add':
+    #         return num1 + num2
+    #     case 'subtract':
+    #         return num1 - num2
+    #     case 'multiply':
+    #         return num2 * num1
+    #     case 'divide':
+    #         try:
+    #             return num1 / num2
+    #         except ZeroDivisionError():
+    #             return f"Can not divide by Zero!"
+    #     case _:
+    #         return f"something went wrong"
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        try:
+            return num1 / num2
+        except ZeroDivisionError():
+            return f"Invalid divide zero divide"
