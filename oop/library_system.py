effc96869d35f0bd56b3701eff117da8092e42dc
HassanElwany <elwany_hassan@hotmail.com>
2024-10-09 18:27:00 +0300

    task 0002

--- oop/library_system.py
+++ oop/library_system.py
@@ -26,13 +26,13 @@ class Book:
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super.__init__(self, title, author)
+        super().__init__(title, author)
         self.file_size = file_size
 
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super.__init__(self, title, author)
+        super().__init__(self, title, author)
         self.page_count = page_count
 
 class Library:
