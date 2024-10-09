076a4f8ee0fec89d788e9acacad4d86426c261b2
HassanElwany <elwany_hassan@hotmail.com>
2024-10-09 17:39:15 +0300

    task 0001

--- oop/library_system.py
+++ oop/library_system.py
@@ -18,3 +18,22 @@
 # Methods:
 # add_book(self, book): Adds a Book, EBook, or PrintBook instance to the library.
 # list_books(self): Prints details of each book in the library.
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super.__init__(self, title, author)
+        self.file_size = file_size
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super.__init__(self, title, author)
+        self.page_count = page_count
+
+class Library:
+    pass
